1f3f3d34bcabc8e36e4f24815c529fd13274fdc7
huhuhang <huhuhang@gmail.com>
2019-01-14 18:04:35 +0800

    add

---
+++ run.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from sklearn.externals import joblib
+import pandas as pd
+import traceback
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Please use the POST method to get prediction results.'
+
+
+@app.route("/", methods=["POST"])  # 请求方法为 POST
+def predict():
+    json_ = request.json  # 解析请求数据
+    query_df = pd.DataFrame(json_)  # 将 json 变为 DataFrame
+    columns_onehot = ["pclass", "sex_female", "sex_male",
+                      "embarked_C", "embarked_Q", "embarked_S"]  # 独热编码 DataFrame 列名
+    query = pd.get_dummies(query_df).reindex(
+        columns=columns_onehot, fill_value=0)  # 将请求数据 DataFrame 处理成独热编码样式
+    clf = joblib.load("titanic.pkl")  # 加载模型
+    prediction = clf.predict(query)  # 模型推理
+    return jsonify({"prediction": list(prediction)})  # 返回推理结果
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
