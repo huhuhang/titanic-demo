bce7b6ac913bb5f32a3d25464809c206a9f36621
huhuhang <huhuhang@gmail.com>
2019-01-15 10:18:28 +0800

    update

--- run.py
+++ run.py
@@ -20,12 +20,12 @@ def predict():
                           "embarked_C", "embarked_Q", "embarked_S"]  # 独热编码 DataFrame 列名
         query = pd.get_dummies(query_df).reindex(
             columns=columns_onehot, fill_value=0)  # 将请求数据 DataFrame 处理成独热编码样式
-        prediction = clf.predict(query)  # 模型推理
-        return jsonify({"prediction": list(prediction)})  # 返回推理结果
+        clf = joblib.load("titanic.pkl")  # 加载模型
+        predictions = clf.predict(query)  # 模型推理
+        return jsonify({"predict": list(predictions)})  # 返回推理结果
     except Exception as e:
         return f"Error: {e}"
 
 
 if __name__ == "__main__":
-    clf = joblib.load("titanic.pkl")  # 加载模型
     app.run(host='0.0.0.0', debug=True)
